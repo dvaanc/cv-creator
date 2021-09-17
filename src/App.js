56a021bf735c947a4710105e659b734ed07c96ae
Danny C PC <dannyc52521@gmail.com>
2021-09-17 18:39:27 +1000

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      val: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmitCV = this.onSubmitCV.bind(this);
+  }
+  handleChange(e) {
+    this.setState({val: e.target.value });
+  }
+  onSubmitCV(e) {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <div className="app">
+
+      </div>
+    )
+  }
 }
 
 export default App;
